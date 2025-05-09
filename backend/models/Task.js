56151e006f1498e461da75ad26f43be6fc6b1f5a
Yash Janbandhu <yashjanbandhu12@gmail.com>
2025-05-09 13:29:50 +0530

    add task model with schema for title, description, status, priority, and userId

---
+++ backend/models/Task.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["incomplete", "complete"],
+      default: "incomplete",
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Low",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
